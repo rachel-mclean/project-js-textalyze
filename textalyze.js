797ff592a0f17ad3d4b81c6da03f853610140191
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-30 13:54:25 -0800

    test

--- textalyze.js
+++ textalyze.js
@@ -6,14 +6,16 @@
 
 // Open lib/itemCounts.js to get started on the first iteration.
 let itemCounts = require('./lib/itemCounts');
+let toArray = require('./lib/toArray');
+let sanitize = require('./lib/sanitize');
 
-let sampleInput = ['a', 'a', 'a', 'b', 'b', 'c'];
+let sampleInput = 'Happy birthday, Rachel!';
 
-let counts = itemCounts(sampleInput);
+let counts = itemCounts(toArray(sanitize(sampleInput)));
 
 // Object.keys returns an array of keys for the given object.
 // See https://javascript.info/keys-values-entries
-for (let item of Object.keys(counts)) {
+for (let item of Object.keys(counts).sort()) {
   let count = counts[item];
 
   console.log(`${item}\t${count}`);
