10b4da9b3b97fa948181d7ba12bff0b7b00a9525
rachel-mclean <59896196+rachel-mclean@users.noreply.github.com>
2020-01-31 14:41:50 -0800

    textalyze

--- textalyze.js
+++ textalyze.js
@@ -5,18 +5,27 @@
 // comments are here to help you, but please delete them as you go along.
 
 // Open lib/itemCounts.js to get started on the first iteration.
-let itemCounts = require('./lib/itemCounts');
+let process = require('process');
+
+// let itemCounts = require('./lib/itemCounts');
 let toArray = require('./lib/toArray');
 let sanitize = require('./lib/sanitize');
+let readFileSync = require('./lib/readFileSync');
+let itemFrequency = require('./lib/itemFrequency');
 
-let sampleInput = 'Happy birthday, Rachel!';
+// let sampleInput = 'Happy birthday, Rachel!';
 
-let counts = itemCounts(toArray(sanitize(sampleInput)));
+// let counts = itemCounts(toArray(sanitize(sampleInput)));
+
+let file = process.argv[2];
+let counts = itemFrequency(toArray(sanitize(readFileSync(file))));
+
+// to run: node textalyze.js sample_data/great-gatsby.txt
 
 // Object.keys returns an array of keys for the given object.
 // See https://javascript.info/keys-values-entries
 for (let item of Object.keys(counts).sort()) {
   let count = counts[item];
-
-  console.log(`${item}\t${count}`);
+  let histogramElement = parseFloat(count) * 5;
+  console.log(`${item}\t${count}` + ' ' + '='.repeat(histogramElement));
 }
